7189809737a506b4c0cc38cd60377ea683f58b18
ChefK-Tech <c27jeff.kwon@afacademy.af.edu>
2026-03-02 20:01:44 -0700

    First Commit

---
+++ src/sevenseg_decoder.vhd
@@ -0,0 +1,62 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/23/2026 09:52:48 AM
+-- Design Name: 
+-- Module Name: sevenseg_decoder - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity sevenseg_decoder is
+    Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
+           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
+end sevenseg_decoder;
+
+architecture Behavioral of sevenseg_decoder is
+
+begin
+    -- The actual "11111111" is inverted from what it actually is as the board is set to active low
+    with i_Hex select
+    o_seg_n <= "0000001" when "0000", -- 0
+            "1001111" when "0001",  -- 1
+            "0010010" when "0010",  -- 2
+            "0000110" when "0011",  -- 3
+            "1001100" when "0100",  -- 4
+            "0100100" when "0101",  -- 5
+            "0100000" when "0110",  -- 6
+            "0001111" when "0111",  -- 7
+            "0000000" when "1000",  -- 8
+            "0000100" when "1001",  -- 9
+            "0001000" when "1010",  -- a  
+            "1100000" when "1011",  -- b
+            "0110001" when "1100",  -- c
+            "1000010" when "1101",  -- d
+            "0110000" when "1110",  -- e
+            "0111000" when "1111",  -- f
+            "1111111" when others;
+
+end Behavioral;
--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -58,6 +58,7 @@ library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
 
+-- Switched all the ports so W7 gets seg[6] (sA) and U7 gets seg[0] (sG)
 
 entity top_basys3 is
 	port(
@@ -66,7 +67,7 @@ entity top_basys3 is
 
 		-- 7-segment display active-low enables (anodes)
 		an      :	out std_logic_vector(3 downto 0);
-
+ 
 		-- Switches
 		sw		:	in  std_logic_vector(3 downto 0);
 		
@@ -79,23 +80,35 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
 	
   -- declare the component of your top-level design
-
+  component sevenseg_decoder is
+     Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
+           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
+     end component sevenseg_decoder;
 
   -- create wire to connect button to 7SD enable (active-low)
-
+  signal w_7SD_EN_n : std_logic;
   
 begin
 	-- PORT MAPS ----------------------------------------
 
 	--	Port map: wire your component up to the switches and seven-segment display cathodes
 	-----------------------------------------------------	
-	
-	
+    uut: sevenseg_decoder port map (
+        i_Hex   => sw,
+        o_seg_n => seg
+     );
+    
 	-- CONCURRENT STATEMENTS ----------------------------
 	
 	-- wire up active-low 7SD anode (active low) to button (active-high)
 	-- display 7SD 0 only when button pushed
 	-- other 7SD are kept off
 	-----------------------------------------------------
+	w_7SD_EN_n  <= not btnC;
+    
+    an(0)   <= w_7SD_EN_n;
+    an(1)   <= '1';
+    an(2)   <= '1';
+    an(3)   <= '1';
 	
 end top_basys3_arch;
